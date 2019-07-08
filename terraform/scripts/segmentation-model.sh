c95ac898d32b023ccf36d584cea40d4063cbd731
archena <archena@users.noreply.github.com>
2019-07-08 12:16:43 +0100

    Uncomment sed lines

--- terraform/scripts/segmentation-model.sh
+++ terraform/scripts/segmentation-model.sh
@@ -12,19 +12,19 @@ DATASET=sahara_marketing
 SED_DATASET=s/__REPLACE_ME__/$PROJECT.$DATASET/g
 
 echo "Segmentation: creating training set view"
-#sed -e $SED_DATASET ../bigquery/sql/segmentation-training-set.sql | bq --location=$LOCATION query
+sed -e $SED_DATASET ../bigquery/sql/segmentation-training-set.sql | bq --location=$LOCATION query
 
 echo "Segmentation: training segmentation model"
-#sed -e $SED_DATASET ../bigquery/sql/segmentation-model.sql | bq --location=$LOCATION query
+sed -e $SED_DATASET ../bigquery/sql/segmentation-model.sql | bq --location=$LOCATION query
 
 echo "Segmentation: model evaluation"
-#sed -e $SED_DATASET ../bigquery/sql/segmentation-evaluation.sql | bq --location=$LOCATION query
+sed -e $SED_DATASET ../bigquery/sql/segmentation-evaluation.sql | bq --location=$LOCATION query
 
 echo "Repeat Buyers: creating training set view"
-#sed -e $SED_DATASET ../bigquery/sql/repeat-buyer-training-set.sql | bq --location=$LOCATION query
+sed -e $SED_DATASET ../bigquery/sql/repeat-buyer-training-set.sql | bq --location=$LOCATION query
 
 echo "Repeat Buyers: training clustering model"
-#sed -e $SED_DATASET ../bigquery/sql/repeat-buyer-model.sql | bq --location=$LOCATION query
+sed -e $SED_DATASET ../bigquery/sql/repeat-buyer-model.sql | bq --location=$LOCATION query
 
 echo "Repeat Buyers: model evaluation"
 sed -e $SED_DATASET ../bigquery/sql/repeat-buyer-evaluation.sql | bq --location=$LOCATION query
