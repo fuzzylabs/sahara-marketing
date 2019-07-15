df186d789e54ac49fb0d924041a7026d2bb2a531
archena <archena@users.noreply.github.com>
2019-07-15 22:42:55 +0100

    More queries for Google Merchandise Store

--- bigquery/sql/new-sql-statements.sql
+++ bigquery/sql/new-sql-statements.sql
@@ -1,4 +1,5 @@
 --------------------------------------------------------------------------------------------------------------------------------
+
 CREATE OR replace VIEW `fuzzylabs.google_merchandise_store.purchases` AS
   SELECT
       userId,
@@ -30,8 +31,9 @@ CREATE OR replace VIEW `fuzzylabs.google_merchandise_store.purchases` AS
       AND productRevenue IS NOT NULL
 
 --------------------------------------------------------------------------------------------------------------------------------
+
 CREATE OR replace VIEW `fuzzylabs.google_merchandise_store.aggregate_purchases` AS
-  SELECT
+SELECT
       fullVisitorId,
       sum(totals.visits) as visits,
       avg(totals.hits) as hits,
@@ -41,7 +43,20 @@ CREATE OR replace VIEW `fuzzylabs.google_merchandise_store.aggregate_purchases` 
       sum(totals.totalTransactionRevenue/1e6) as total_revenue,
       sum(product.productQuantity) AS product_quantity,
       sum(product.productRevenue/1e6) AS product_revenue,
-      sum(totals.transactions) > 1 AS repeat_buyer
+      sum(totals.transactions) > 1 AS repeat_buyer,
+      max(geoNetwork.country) as country,
+      -- mocked ages: we suppose repeat customers are aged between 20-35 in uniform random distribution
+      -- non-repeat buyers overlap slightly, falling in the 32-52 range
+      case when sum(totals.transactions) > 1 then
+        round(20 + rand() * 15, 0)
+      else
+        round(32 + rand() * 20, 0)
+      end as age,
+      case when rand() < 0.5 then
+        "Male"
+      else
+        "Female"
+      end as gender
   FROM
       `bigquery-public-data.google_analytics_sample.ga_sessions_*`
       , UNNEST(hits) AS hits
@@ -56,37 +71,101 @@ CREATE OR replace VIEW `fuzzylabs.google_merchandise_store.aggregate_purchases` 
 
 -- create model
 --------------------------------------------------------------------------------------------------------------------------------
+
 CREATE OR REPLACE MODEL `fuzzylabs.google_merchandise_store.repeat_purchase_model`
 OPTIONS(model_type='logistic_reg') AS
 SELECT
     repeat_buyer AS label,
     visits,
     pageviews,
-    time_on_site
+    time_on_site,
+    age,
+    gender
 FROM
  `fuzzylabs.google_merchandise_store.aggregate_purchases`
 
--- evaluate
+-- evaluate repeat buyer model
 --------------------------------------------------------------------------------------------------------------------------------
+
 SELECT * FROM
 ML.EVALUATE(MODEL `fuzzylabs.google_merchandise_store.repeat_purchase_model`, (
   select
     repeat_buyer AS label,
     visits,
     pageviews,
-    time_on_site
+    time_on_site,
+    age,
+    gender
   from `fuzzylabs.google_merchandise_store.aggregate_purchases`
 ))
 
--- query
+-- query repeat buyer model
 --------------------------------------------------------------------------------------------------------------------------------
-SELECT 
+SELECT
 *
-FROM 
+FROM
  ML.PREDICT(
   MODEL `fuzzylabs.google_merchandise_store.repeat_purchase_model`,
   TABLE `fuzzylabs.google_merchandise_store.aggregate_purchases`
 )
 
-
+-- What do repeat buyers buy?
 --------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE VIEW `fuzzylabs.google_merchandise_store.repeat_buyer_purchases` AS SELECT
+repeat_buyer,
+aggregate_purchases.fullVisitorId,
+aggregate_purchases.visits,
+aggregate_purchases.pageviews,
+aggregate_purchases.time_on_site,
+aggregate_purchases.total_transactions,
+purchases.visitId,
+purchases.product_name,
+purchases.product_category
+FROM
+`fuzzylabs.google_merchandise_store.aggregate_purchases` aggregate_purchases,
+`fuzzylabs.google_merchandise_store.purchases` purchases
+WHERE
+repeat_buyer = true and
+purchases.fullVisitorId = aggregate_purchases.fullVisitorId;
+
+-- segmentation model
+--------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE MODEL `fuzzylabs.google_merchandise_store.customer_segments`
+OPTIONS(model_type='kmeans', num_clusters=2, distance_type='euclidean') AS
+SELECT
+    repeat_buyer,
+    visits,
+    pageviews,
+    time_on_site,
+    age,
+    gender
+FROM
+ `fuzzylabs.google_merchandise_store.aggregate_purchases`
+
+-- evaluate segmentation
+--------------------------------------------------------------------------------------------------------------------------------
+
+SELECT * FROM
+ML.EVALUATE(MODEL `fuzzylabs.google_merchandise_store.customer_segments`, (
+  select
+    repeat_buyer,
+    visits,
+    pageviews,
+    time_on_site,
+    age,
+    gender
+  from `fuzzylabs.google_merchandise_store.aggregate_purchases`
+))
+
+-- query segments
+--------------------------------------------------------------------------------------------------------------------------------
+
+SELECT
+*
+FROM
+ ML.PREDICT(
+  MODEL `fuzzylabs.google_merchandise_store.customer_segments`,
+  TABLE `fuzzylabs.google_merchandise_store.aggregate_purchases`
+)
